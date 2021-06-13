a6af1a7587755d6143167258d53731ae5fe73a5b
Aman <av36232@gmail.com>
2021-06-13 19:28:42 +0530

    pdf

--- lib/PDF_Screens/pdf_creator_screen.dart
+++ lib/PDF_Screens/pdf_creator_screen.dart
@@ -7,26 +7,87 @@ class PDFCreator extends StatefulWidget {
 }
 
 class _PDFCreatorState extends State<PDFCreator> {
+  String pdfText;
+  String pdfHeading;
+  String pdfName;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Container(
-              margin: EdgeInsets.only(top: 30),
-              height: 40,
-              child: RaisedButton(
-                  shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(5.0),
+        child: Container(
+          margin: EdgeInsets.only(top: 30),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        labelText: 'Enter Pdf Header',
+                        hintText: 'PDF Header'),
+                    keyboardType: TextInputType.multiline,
+                    minLines: 1, //Normal textInputField will be displayed
+                    maxLines: 2, // when user presses enter it will adapt to it
+                    onChanged: (value) {
+                      pdfHeading = value;
+                    },
                   ),
-                  child: Text(
-                    'Get Report',
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        labelText: 'Enter Pdf Text',
+                        hintText: 'PDF Text'),
+                    keyboardType: TextInputType.multiline,
+                    minLines: 1, //Normal textInputField will be displayed
+                    maxLines: 100,
+                    onChanged: (value) {
+                      pdfText = value;
+                    },
                   ),
-                  color: Colors.blue,
-                  onPressed: () {
-                    reportView(context);
-                  }))),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        labelText: 'Enter Pdf Name',
+                        hintText: 'PDF Name'),
+                    onChanged: (value) {
+                      pdfName = value;
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        foregroundColor: MaterialStateProperty.all(Colors.red),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Colors.red)))),
+                    child: Text(
+                      'Create PDF',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      reportView(context, pdfHeading, pdfText, pdfName);
+                    }),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/PDF_Screens/report_pdf.dart
+++ lib/PDF_Screens/report_pdf.dart
@@ -1,17 +1,20 @@
 import 'dart:io';
-
 import 'package:elasticsearch_project/PDF_Screens/pdf_viewer_page.dart';
 import 'package:flutter/material.dart' as material;
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
+import 'package:flutter/services.dart';
 import 'package:pdf/widgets.dart';
 
-reportView(context) async {
+reportView(context, heading, text, pdfName) async {
   final Document pdf = Document();
-
-  pdf.addPage(MultiPage(
-      pageFormat:
-          PdfPageFormat.letter.copyWith(marginBottom: 1.5 * PdfPageFormat.cm),
+  final font = await rootBundle.load("assets/Lato-Regular.ttf");
+  final ttf = Font.ttf(font);
+  pdf.addPage(
+    MultiPage(
+      // pageFormat:
+      //     PdfPageFormat.letter.copyWith(marginBottom: 1.5 * PdfPageFormat.cm),
+      pageFormat: PdfPageFormat.a4,
       crossAxisAlignment: CrossAxisAlignment.start,
       header: (Context context) {
         if (context.pageNumber == 1) {
@@ -22,9 +25,12 @@ reportView(context) async {
             margin: EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
             padding: EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
             decoration: BoxDecoration(
-                border:
-                    BoxBorder(bottom: true, width: 0.5, color: PdfColors.grey)),
-            child: Text('Report',
+              border: Border.all(
+                width: 8,
+              ),
+            ),
+            // Border(bottom: true, width: 0.5, color: PdfColors.grey)),
+            child: Text(heading,
                 style: Theme.of(context)
                     .defaultTextStyle
                     .copyWith(color: PdfColors.grey)));
@@ -39,46 +45,23 @@ reportView(context) async {
                     .copyWith(color: PdfColors.grey)));
       },
       build: (Context context) => <Widget>[
-            Header(
-                level: 0,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text('Report', textScaleFactor: 2),
-                      PdfLogo()
-                    ])),
-            Header(level: 1, text: 'What is Lorem Ipsum?'),
-            Paragraph(
-                text:
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
-            Paragraph(
-                text:
-                    'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'),
-            Header(level: 1, text: 'Where does it come from?'),
-            Paragraph(
-                text:
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
-            Paragraph(
-                text:
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
-            Padding(padding: const EdgeInsets.all(10)),
-            Table.fromTextArray(context: context, data: const <List<String>>[
-              <String>['Year', 'Ipsum', 'Lorem'],
-              <String>['2000', 'Ipsum 1.0', 'Lorem 1'],
-              <String>['2001', 'Ipsum 1.1', 'Lorem 2'],
-              <String>['2002', 'Ipsum 1.2', 'Lorem 3'],
-              <String>['2003', 'Ipsum 1.3', 'Lorem 4'],
-              <String>['2004', 'Ipsum 1.4', 'Lorem 5'],
-              <String>['2004', 'Ipsum 1.5', 'Lorem 6'],
-              <String>['2006', 'Ipsum 1.6', 'Lorem 7'],
-              <String>['2007', 'Ipsum 1.7', 'Lorem 8'],
-              <String>['2008', 'Ipsum 1.7', 'Lorem 9'],
-            ]),
-          ]));
+        Header(
+            level: 0,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(heading, textScaleFactor: 2),
+                  PdfLogo()
+                ])),
+        Header(level: 1, text: heading, outlineStyle: PdfOutlineStyle.normal),
+        Paragraph(text: text, style: TextStyle(font: ttf))
+      ],
+    ),
+  );
   //save PDF
 
   final String dir = (await getApplicationDocumentsDirectory()).path;
-  final String path = '$dir/report.pdf';
+  final String path = '$dir/' + pdfName;
   final File file = File(path);
   await file.writeAsBytes(pdf.save());
   material.Navigator.of(context).push(
